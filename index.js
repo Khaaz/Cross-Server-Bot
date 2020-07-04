6b0e38ccba9252cdf1e1dcf91b98fa4716a2485f
Khaazz <khaaz.dev@gmail.com>
2020-07-04 15:41:30 +0200

    feat: global listeners to catch errors

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const config = require('./config.json');
 const { bot } = require('./src/bot');
 const { setup } = require('./src/index');
 
+function formatDate() {
+    const date = new Date();
+
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} - ${date.getHours()}:${date.getMinutes()}`;
+}
+
 setup(bot, config);
 
 bot.connect();
+
+bot.on('error', (err) => console.log(`${formatDate()} : ${err.stack || err.message}`) );
+bot.on('warn', (msg) => console.log(`${formatDate()} : ${msg}`) );
